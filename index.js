a4ff3fc1adc9e245e5b85dce01696b5f7af64dc8
Levin-ops <artmwanganyi09@outlook.com>
2023-10-01 12:25:07 +0300

    Phase 1 JS Events Lab

--- index.js
+++ index.js
@@ -1,6 +1,47 @@
 function addingEventListener() {
     const input=document.getElementById(`button`);
     input.addEventListener(`click`,function(){
-        alert(`I was clicked`)
+        alert(`I was clicked On the Main`)
     })
 }
+addingEventListener();
+
+function divEventListenera(){
+    const event=document.getElementById(`button2`)
+    event.addEventListener(`click`, function(){
+        alert(`Mandem You Clicked on Div 1`)
+    })
+}
+divEventListenera();
+
+function divEventListenerb(){
+    const event = document.getElementById(`button3`)
+    event.addEventListener(`click`,function(){
+        alert(`Okay I am button with input ID button 3`)
+    })
+}
+divEventListenerb()
+
+function divEventListenerc(){
+    const event=document.getElementById(`button4`)
+    event.addEventListener(`click`, function(){
+        alert(`I am button number 4`)
+    })
+}
+divEventListenerc()
+
+function divEventListenerd(){
+    const event=document.getElementById(`button5`)
+    event.addEventListener(`click`,function(){
+        alert(`I am the last JEDI`)
+    })
+}
+divEventListenerd()
+
+function divEventListenere(){
+    const event=document.getElementById(`button6`)
+    event.addEventListener(`click`,function(){
+        alert(`I am the one and only DARTH VADER!!!! `)
+    })
+}
+divEventListenere()
